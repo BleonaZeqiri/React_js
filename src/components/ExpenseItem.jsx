ab613f912209eda234ef0e076cfe99e50338863f
Bleona <zbleona9@gmail.com>
2024-07-25 02:21:14 +0200

    Added ExpenseDate

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,3 +1,4 @@
+import ExpenseDate from "./ExpenseDate.jsx";
 import "./ExpenseItem.css";
 function ExpenseItem(props) {
   // const expenseDate = new Date(2021, 2, 28);
@@ -5,7 +6,7 @@ function ExpenseItem(props) {
   // const expenseAmount = 300.0;
   return (
     <div className="expense-item ">
-      <div> {props.date.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
